5c25a3bcf16b999d74c8766d76e8667a92ddc5a6
Chris-Howard <1737057149@qq.com>
2022-04-13 20:56:33 +0800

    Update tranffic_light.rs

--- src/tranffic_light.rs
+++ src/tranffic_light.rs
@@ -1,16 +1,11 @@
-fn main() {
-    println!("红灯时间：{}，黄灯时间{},绿灯时间{}",TrafficLight::Red.time(),TrafficLight::Yellow.time(),TrafficLight::Green.time());
-}
-
-
-enum TrafficLight{
+pub enum TrafficLight{
     Red,
     Yellow,
     Green,
 }
 
 impl TrafficLight{
-    fn time(&self)->u8{
+    pub(crate) fn time(&self) ->u8{
         return match self {
             TrafficLight::Red => 15,
             TrafficLight::Yellow => 10,
